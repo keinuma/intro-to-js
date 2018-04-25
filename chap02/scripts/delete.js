b764301542e4c3ad0681edd6892bef7861c7c6d4
numata <nununu.mono@gmail.com>
2018-04-25 21:28:21 +0900

    delete Arrayはindexが残る

---
+++ chap02/scripts/delete.js
@@ -0,0 +1,11 @@
+var ary = ['JavaScript', 'Ajax', 'ASP.NET'];
+console.log(delete ary[0]);
+console.log(ary);
+
+var data1 = 1;
+console.log(delete data1);
+console.log(data1);
+
+data2 = 10;
+console.log(delete data2);
+console.log(data2);
