4e6c0b74d80e0c4f96cf622ec73d1f2a5f68b99a
numata <nununu.mono@gmail.com>
2018-04-25 21:37:13 +0900

    arrayのtypeはobject

---
+++ chap02/scripts/typeof.js
@@ -0,0 +1,11 @@
+var num = 1;
+console.log(typeof num);
+
+var flag = true;
+console.log(typeof flag);
+
+var ary = ['JavaScript', 'Ajax', 'ASP.NET'];
+console.log(ary);
+
+var obj = {x: 1, y: 2};
+console.log(typeof obj);
