43de8dc62cfc6a43286ee8b127756e26c083a96b
numata <nununu.mono@gmail.com>
2018-05-31 14:10:29 +0900

    Htmlの要素はdocumentオブジェクトから取得、テキストはtextContentからアクセス

---
+++ chap06/6-2/scripts/element_id.js
@@ -0,0 +1,4 @@
+let current = new Date();
+let result = document.getElementById('result');
+
+result.textContent = current.toLocaleString();
---
+++ chap06/6-2/scripts/element_name.js
@@ -0,0 +1,3 @@
+let current = new Date();
+let nam = document.getElementsByName('time');
+nam[0].value = current.toLocaleTimeString();
